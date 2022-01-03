ab4cb766cfe9ada69d2fe5a9b2a2c159bdbe2dc7
Baldwin, Josiah <jbaldwin@nwvariancesoftware.dev>
2022-01-02 16:11:02 -0800

    Merged expection source into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,46 @@
 //! This macro library is currently incomplete, it works for enums in a very
 //! specific format but it is currently very touchy.
 //! Use at your own risk.
+use std::error::Error;
+use std::fmt;
+
 pub use try_from_derive_proc::TryFromVariants;
 
-mod variant_cast_error;
+/// Different variant than expected during TryFrom
+#[derive(Debug)]
+pub struct VariantCastError {
+    /// Enum type
+    pub enum_type: &'static str,
+    /// Expected variant
+    pub exp_type: &'static str,
+    /// Actual variant
+    pub variant_name: &'static str,
+}
 
-pub use variant_cast_error::*;
+impl fmt::Display for VariantCastError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Casting {} to {} failed, variant was {}.",
+            self.enum_type, self.exp_type, self.variant_name
+        )
+    }
+}
+
+impl Error for VariantCastError {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn expected_display_string() {
+        let error = VariantCastError {
+            enum_type: "Carl",
+            exp_type: "Fish",
+            variant_name: "Bread",
+        };
+        let displayed = format!("{}", error);
+        assert_eq!("Casting Carl to Fish failed, variant was Bread.", displayed);
+    }
+}
