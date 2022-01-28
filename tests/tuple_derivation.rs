f7c73bb10a977c86336bfeb1cc747e20a57da307
Baldwin, Josiah <jbaldwin@nwvariancesoftware.dev>
2022-01-28 11:45:27 -0800

    Tuple derivation test

--- tests/tuple_derivation.rs
+++ tests/tuple_derivation.rs
@@ -1,31 +1,31 @@
 use enum_variant_macros::*;
-use strum_macros::IntoStaticStr;
 use std::error::Error;
+use strum_macros::IntoStaticStr;
 
 #[derive(PartialEq, Debug, IntoStaticStr, FromVariants, TryFromVariants)]
 enum Variants {
-	Int(i32),
-	Point(f32),
+    Int(i32),
+    Point(f32, f32),
 }
 
 #[test]
 fn tuple_from_try_from() -> Result<(), Box<dyn Error>> {
-	let source = Variants::Point(12.0);
-	let expected = (12.0);
-	let result: f32 = TryFrom::try_from(source)?;
-	assert_eq!(expected, result);
+    let source = Variants::Point(12.0, 12.0);
+    let expected = (12.0, 12.0);
+    let result: (f32, f32) = TryFrom::try_from(source)?;
+    assert_eq!(expected, result);
 
-	let source = Variants::Int(122);
-	let result = f32::try_from(source);
-	assert!(result.is_err());
+    let source = Variants::Int(122);
+    let result = f32::try_from(source);
+    assert!(result.is_err());
 
-	Ok(())
+    Ok(())
 }
 
 #[test]
 fn tuple_to_variant() {
-	let source = (12.0);
-	let expected = Variants::Point(12.0);
-	let result = Variants::from(source);
-	assert_eq!(expected, result);
+    let source = (12.0, 12.0);
+    let expected = Variants::Point(12.0, 12.0);
+    let result = Variants::from(source);
+    assert_eq!(expected, result);
 }
